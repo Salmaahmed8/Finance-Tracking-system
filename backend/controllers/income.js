968507de9579949b5bce534680ea2db718247986
Salma Ahmed <108466703+Salmaahmed8@users.noreply.github.com>
2025-01-09 00:55:30 +0200

    feat: add delete income function

--- backend/controllers/income.js
+++ backend/controllers/income.js
@@ -29,3 +29,26 @@ exports.addIncome = async (req, res) => {
 
     console.log(income)
 }
+
+exports.getIncomes = async (req, res) =>{
+    try {
+        const incomes = await IncomeSchema.find({ user: req.user._id }).sort({createdAt: -1})
+        res.status(200).json(incomes)
+    } catch (error) {
+        res.status(500).json({message: 'Server Error'})
+    }
+}
+
+exports.deleteIncome = async (req, res) =>{
+    const {id} = req.params;
+    IncomeSchema.findByIdAndDelete({ _id: id, user: req.user._id })
+    .then((income) => {
+        if (!income) {
+          return res.status(404).json({ message: 'Income not found or unauthorized!' });
+        }
+        res.status(200).json({ message: 'Income Deleted' });
+      })
+        .catch((err) =>{
+            res.status(500).json({message: 'Server Error'})
+        })
+}
--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,5 +1,7 @@
 const {
-    addIncome
+    addIncome,
+    getIncomes,
+    deleteIncome
 } = require('../controllers/income');
 
 const router = require('express').Router();
@@ -7,6 +9,9 @@ const protect = require('../middleware/authMiddleware');
 
 
 router.post('/add-income'. protect, addIncome);
+router.get('/get-incomes', protect, getIncomes);
+router.delete('/delete-income/:id', protect, deleteIncome)
+
 
 
 module.exports = router;
